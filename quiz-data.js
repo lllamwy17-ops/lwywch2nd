f1e84a4f37ac24904a9d45b5cfb99de84d8eb15b
lllamwy17-ops <lllamwy17@gmail.com>
2026-09-20 21:41:55 +0800

    ok

---
+++ quiz-data.js
@@ -0,0 +1,57 @@
+const quizQuestions = [
+  {
+    question: "1. Where did we go on our first anniversary date?",
+    options: ["A) Cheung Chau", "B) Shenzhen", "C) Kai Tak", "D) TWW"],
+    correctIndex: 2
+  },
+  {
+    question: "2. What is my favorite thing you did for me in Year 2?",
+    options: ["A) Driving me home", "B) Saying that Im cute, Saying that you love me, and then hug me, and then kiss me", "C) Buying gifts/ food to me", "D) Apologising to me"],
+    correctIndex: 1
+  },
+  {
+    question: "3. Deep down, what I fear most is that...",
+    options: ["A) you don't trust me.", "B) you don't play with me.", "C) you cheat on me.", "D) you don't think I'm the best girl in the world."],
+    correctIndex: 3
+  },
+  {
+    question: "4. When I am sad/ angry, I want you to...",
+    options: ["A) tum me and take the initiative to actually do sth.", "B) ignore me and pretend nth happens.", "C) ask me what I want.", "D) tell me I am too sensitive."],
+    correctIndex: 0
+  },
+  {
+    type: "open",
+    question: "5. What did I do that makes you feel that I love you? I feel I am loved when 你在我奇奇怪怪的時候還說我可愛，我就覺得被你愛著。",
+    placeholder: "Type your response here..."
+  },
+  {
+    type: "open",
+    question: "6. What did I do that makes you feel that you love me? I feel I love you when 當你做什麼都有想到我，當你想跟我在一起，當你即使很累也會幫我拿東西/車我回家，我就會有想嫁給你的感覺",
+    placeholder: "Type your response here..."
+  },
+  {
+    type: "open",
+    question: "7. Do you get sick of me being too sensitive? Did I do better this year? ;) My answer is: 我真的要承認自己非常有佔有慾，我覺得今年的我比去年的我好了一丟丟，你好像沒有哭那麼多次...",
+    placeholder: "Type your response here..."
+  },
+  {
+    type: "open",
+    question: "8. What do you imagine our future to be like? My answer is: 未來幾年年我們一起奮鬥，你會去到更好的學校，我會努力穩固我在這裡的地位。我們兩個會越來越成熟，但在彼此面前還是很幼稚。將來的我們會一年旅行幾次。當我們有個小窩居，我們會有貓（不知道有沒有狗)(update: 我的天哪,在我製作這個的時候你已經有狗狗了)，我會嫌棄你不會做家務，但你會學，你會買洗碗機和掃地機器人給我哈哈哈。",
+    placeholder: "Type your response here..."
+  },
+  {
+    type: "open",
+    question: "9. Write down one thing you want to do with me next year. My answer is: 海傍拍拖",
+    placeholder: "Type your response here..."
+  },
+  {
+    type: "open",
+    question: "10. Sometimes, you seem really afraid of me. Do you really feel safe and comfortable with me?",
+    placeholder: "Type your response here..."
+  },
+  {
+    question: "11. Are you ready to step into our third year together?",
+    options: ["A) Yes!", "B) No."],
+    correctIndex: 0
+  }
+];
---
+++ timeline-data.js
@@ -0,0 +1,194 @@
+const memoryTimeline = [
+  {
+    date: "Day 1 of Year 2",
+    title: "Our Anniversary Celebration",
+    message: "Starting year two together! Loved the flower you picked out. 上天也為我們高興，給了我們一間好房間，那天感覺像去旅行了。",
+    photos: [
+      "images/IMG_1773.JPG",
+      "images/IMG_1816.JPG"
+    ], 
+    audio: "",
+    youtubeId: ""
+  },
+  {
+    date: "17 Nov 2025 & Christmas",
+    title: "My Birthday & Christmas",
+    message: 'I still have the "money flower" with me, I love when you 為我花心思, 我最喜歡驚喜了嘻嘻～ 其實以前的我不擅長新鮮感（？）但我覺得返工之後，新鮮感這的很重要，所以我會想好多小遊戲跟你玩>3< 超喜歡跟你兩個人玩麻雀喔！',
+    photos: [
+      "images/money.JPG",
+      "images/IMG_2339.JPG",
+      "images/IMG_3321.JPG",
+      "images/IMG_2334.jpg"
+    ],
+    audio: "",
+    youtubeIds: [
+      "_3uy0V0anec"
+    ]
+  },
+  {
+    date: "22 Nov 2025",
+    title: "不詳",
+    message: "冬天好幸福嘻嘻，因為可以同你貼貼～期待期待。",
+    photos: [
+      "images/IMG_2704.jpg"
+    ],
+    audio: "",
+    youtubeIds: ""
+  },
+  {
+    date: "8 Dec 2025",
+    title: "Ocean Park",
+    message: "叻叻豬個日贏左lulu豬，最後有人嬲豬豬。我地係兩個極端，一個極鵪鶉，一個勁易爆，我們兩個都不好。最體面嘅做法係我地都可以心平氣和咁同對方解決問題。I will try!",
+    photos: [
+      "images/IMG_2971.jpg",
+      "images/IMG_2977.jpg",
+      "images/IMG_2985.jpg"
+    ],
+    audio: "",
+    youtubeIds: ""
+  },
+  {
+    date: "16 Dec 2025",
+    title: "Your family BBQ",
+    message: "thank you and your family for including me <3 like seeing you being childish (sometimes) 所以其實我也不是很想要一個非常成熟的男人啊",
+    photos: [
+      "images/IMG_3112.jpg"
+    ],
+    audio: "",
+    youtubeIds: ""
+  },
+  {
+    date: "12 Jan 2026",
+    title: "Awwww Linabell",
+    message: "謝謝寶貝的驚喜，家裡已經沒有位置放毛公仔啦QAQ 等我搬新屋再買可以嗎謝謝！",
+    photos: [
+      "images/IMG_3821.jpg"
+    ], 
+    audio: "",
+    youtubeId: ""
+  },
+  {
+    date: "19 Jan 2026",
+    title: "TAT",
+    message: "還記得那天嗎...你欺騙我沒有自己來！！你知道我是很單純的嗎！！我很相信你欸！！個次算是我嬲最耐嘅一次吧。誠信是最重要的ok!! 雖然宜家諗返，其實自己黎真係冇我想像中咁worse...只能說...呢方面，我地有待繼續磨合...",
+    photos: [
+      "images/IMG_3949.jpg"
+    ], 
+    audio: "",
+    youtubeId: ""
+  },
+  {
+    date: "Valentine's Day",
+    title: "恭喜你有自己的小狗狗！！",
+    message: "我覺得對小動物好的人一定都是好人，現在你有了自己的小狗狗，讓我看到你有愛心、有責任、成熟的一面吧！I know it's a huge change and responsibility for you. Believe me, it will bring more happiness than that to you <3 <3 I also believe you will be a good owner (with my help hehe)",
+    photos: [
+      "images/IMG_1137.JPG"
+    ], 
+    audio: "",
+    youtubeId: ""
+  },
+  {
+    date: "15/02/2026",
+    title: "PS5555555",
+    message: "我的男孩我來寵@@ 還好你那天沒有發我脾氣喔，但我的確不應該在你不想出門的時候硬要你渣車出黎。可惜買完沒有什麼時間一起玩。enjoy your GTA la! ;)",
+    photos: [
+      "images/IMG_4879.JPG"
+    ], 
+    audio: "",
+    youtubeId: ""
+  },
+  {
+    date: "Feb 2026",
+    title: "Lunar New Yearrrr",
+    message: "啊蔡太，我都快忘記這事了。你猜她說的話準嗎？我地都踏入第二年了哈哈，得閒再揾佢「算帳」，講笑。",
+    photos: [
+      "images/IMG_4894.jpg",
+      "images/IMG_4931.jpg",
+      "images/IMG_4391.jpg",
+      "images/newyear.JPG"
+    ],
+    audio: "",
+    youtubeIds: ""
+  },
+  {
+    date: "不詳",
+    title: "不詳",
+    message: "a random clip that I like",
+    photos: [], 
+    audio: "",
+    youtubeId: "RhSayhdMDQA"
+  },
+  {
+    date: "不詳",
+    title: 'dream "scene" with you',
+    message: "save了很久的clip，是我理想中的畫面，下次我們一起去海邊的城市/國家玩好嗎😛",
+    photos: [], 
+    audio: "",
+    youtubeId: "HWLxdWViQko"
+  },
+  {
+    date: "不詳",
+    title: "你給我的日本手信",
+    message: "被惦記的感覺真好，但是7天真的好久喔",
+    photos: [
+      "images/IMG_5775.jpg"
+    ], 
+    audio: "",
+    youtubeId: ""
+  },
+  {
+    date: "01/04/2026,April Fools(?",
+    title: "深圳3日2夜",
+    message: "this is so fun, we are so cute",
+    photos: [
+      "images/IMG_6096.jpg"
+    ], 
+    audio: "",
+    youtubeId: ""
+  },
+  {
+    date: "23/06/2026",
+    title: "Video Call",
+    message: "喜歡跟你碎碎念，你喜歡我碎碎念嗎。我覺得你現在也會碎碎念了。",
+    photos: [
+      "images/ec12e54b-3cc2-4bf4-9419-62e23a77b373.JPG"
+    ], 
+    audio: "",
+    youtubeId: ""
+  },
+  {
+    date: "11 July 2026",
+    title: "JFFT...",
+    message: "Im really really sorry about that day. 講真實的原因你真的會覺得我是變態控制狂吧...個日係無啦啦跳出一個女的，看著好做作，看不下去，結果你也選了她，我就無語了。下次你都係先俾個headsup我，介紹下d人，了解左你點解鍾意嘅話我可能比較能理解。anyway... I could handle it better... but I felt too ashamed to say anything...",
+    photos: [
+      "images/IMG_8254.jpg"
+    ],
+    audio: "",
+    youtubeId: ""
+  },
+  {
+    date: "Summer Vacation",
+    title: "Fukuoka Trip!",
+    message: "首先，要感謝你駕駛。I will never never take it for granted, mark my words!! 我感謝你 numerically車我翻屋企，謝謝你寶貝！很開心可以見證你第一次買車，仲有第一次自駕游，你真的很棒喔！！記住，我只要求你渣車要安全平安，行錯路is fine，只要安全駕駛就好。在糸島的那次，我完全不遺憾，我見到我想見嘅野，換個角度諗，呢次“小意外”算是另一種回憶吧哈哈，人沒事就好。還記得上年同你去韓國，回程就哭了，除左因為未去過歐洲（宜家根本唔知有咩好喊），更因為同你旅行太美好了，回來香港的時候都依依不捨。（但你不覺得，你就是很想回家對嗎哼哼！！）這次我沒有哭（就連那個拍得我們一點都不好看又貴的攝影師，我都沒有哭）因為我知道我地好快就會有下一次!",
+    photos: [
+      "images/IMG_2763.jpg",
+      "images/IMG_0175.jpg",
+      "images/IMG_0178.jpg",
+      "images/IMG_0191.JPG",
+      "images/IMG_3356.JPG",
+      "images/DSCF8113.JPEG",
+      "images/DSCF8055.JPEG",
+      "images/DSCF7985.JPEG",
+      "images/IMG_9906.jpg",
+      "images/IMG_1354.JPG"
+    ],
+    audio: "",
+    youtubeIds: [
+      "hwa_nWfdAE4",
+      "pAyFyDJebN8",
+      "F--D7jjlZXc",
+      "pjqJT3Hzicg",
+      "8U9EjfLUqsA"
+    ]
+  }
+];
